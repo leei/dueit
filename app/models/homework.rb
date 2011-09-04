c28d76e3979fb459119ecb151e154e5ac7edc88b
Maxwell Iverson <maxtheax@live.ca>
2011-09-05 03:15:31 +0800

    Add due date to header (untested)

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -5,4 +5,19 @@ class Homework < ActiveRecord::Base
   validates :name, :length => 3..140
 
   validates :deadline, :presence => true
+
+  def how_soon
+    soon = deadline - Date.now
+    if soon < 0
+      :overdue
+    elsif soon < 1.day
+      :tomorrow
+    elsif soon < 2.days
+      :two_days
+    elsif soon < 1.week
+      :week
+    else
+      :not_yet
+    end
+  end
 end
