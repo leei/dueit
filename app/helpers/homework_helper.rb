99c4a873817b13642d809f61ccc98a9065151f56
Maxwell Iverson <poseidon101@telus.net>
2011-09-08 16:07:02 -0700

    Move status update form to helper

--- app/helpers/homework_helper.rb
+++ app/helpers/homework_helper.rb
@@ -1,2 +1,10 @@
 module HomeworkHelper
+  def update_homework_state(homework, string, state)
+    form_id = "state_#{state}_#{homework.id}"
+    capture do
+      form_for(homework, :html => {:id => form_id}) do |f|
+        concat f.hidden_field(:state, :value => "done")
+      end
+    end + link_to_function(string, "$('##{form_id}').submit()")
+  end
 end
